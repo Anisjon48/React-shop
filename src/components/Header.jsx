8a724089cf0662d94e9329c0c013af8246c63e65
Anisjon413 <anisakhmedov06@gmail.com>
2022-11-29 20:13:17 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
 	return <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">React Shop</a>
+      <a href="https://github.com/Anisjon48/React-shop" target ="_blank" rel="noreferrer" class="brand-logo">React Shop</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
